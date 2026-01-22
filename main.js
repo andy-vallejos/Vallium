6902dc1f09ef1448c530fad9c156483cf398db7f
Andy Vallejos <andyvallejosgb@gmail.com>
2026-01-22 15:03:52 -0400

    add React renderer with vite

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(__dirname, "renderer/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
